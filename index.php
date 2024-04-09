33be0330afaf517de39ec666142a22eb89776b00
GamorySensei <theo.gamory@metznumericschool.fr>
2024-04-09 11:59:47 +0200

    Traitement du formulaire de mise à jour des états des tâches

--- index.php
+++ index.php
@@ -5,6 +5,32 @@ require 'classes/Task.php';
 // Récupération des tâches en BDD
 require 'includes/db-connect.php';
 
+// On vérifie si le formulaire est envoyé et on traite les tâches cochées
+if (isset($_POST['submit_task_form'])) {
+    // Tâches cochées
+    $checkedTasks = !empty($_POST['checked']) ? $_POST['checked'] : [];
+
+    if (!empty($checkedTasks)) {
+        // Màj des tâches
+        // Création de la clause IN pour la requête
+        $placeholders = implode(',', array_fill(0, count($checkedTasks), '?'));
+
+        // On marque comme complétée les tâches	cochées
+        $query = $db->prepare("UPDATE task SET task_completed_at = NOW() WHERE task_id IN ($placeholders) AND task_completed_at IS NULL");
+        $query->execute(array_values($checkedTasks));
+
+        // On supprime la date de complétion si la tâche est décochée
+        $query = $db->prepare("UPDATE task SET task_completed_at = NULL WHERE task_id NOT IN ($placeholders)");
+        $query->execute(array_values($checkedTasks));
+    }
+    else
+    {
+        // On remet toutes les tâches comme non complétée
+        $query = $db->query("UPDATE task SET task_completed_at = NULL");
+    }
+
+}
+
 $query = $db->query("SELECT task_id, title_task, description_task, task_created_at, task_completed_at FROM task");
 $taskCollection = $query->fetchAll();
 
@@ -12,17 +38,13 @@ $taskCollection = $query->fetchAll();
 $tasks = [];
 foreach ($taskCollection as $task) {
     $tasks[] = new Task(
-        $id = $task['task_id'], 
-        $title = $task['title_task'], 
+        $id = $task['task_id'],
+        $title = $task['title_task'],
         $description = $task['description_task'],
         $createdAt = new DateTime($task['task_created_at']),
         $completedAt = $task['task_completed_at'] ? new DateTime($task['task_completed_at']) : null,
     );
 }
-
-// On vérifie si le formulaire est envoyé et on traite les tâches cochées
-
-
 
 
 
@@ -67,7 +89,7 @@ foreach ($taskCollection as $task) {
             </tbody>
         </table>
 
-        <input type="submit" name="submit" value="Mettre à jour">
+        <input type="submit" name="submit_task_form" value="Mettre à jour">
     </form>
 
     <!-- Bootstrap JavaScript Libraries -->
